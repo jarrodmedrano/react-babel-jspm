b4e38582bacb0ac6555a5d16a7f8d915c3b12b12
jmedran <jmedran@gmail.com>
2015-10-29 17:11:24 -0700

    refactoring

---
+++ src/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import React from 'react';
+
+import Hero from './components/Hero.js!jsx';
+
+React.render(
+  React.createElement(Hero),
+  document.getElementById('app')
+);
---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import React from 'react';
+
+export class Hero extends React.Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        );
+    }
+}
+
+export default Hero;
