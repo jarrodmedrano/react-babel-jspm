8b7a07d3244494e4030ffb1af849daaed80bdf08
jmedran <jmedran@gmail.com>
2015-11-08 20:30:37 -0800

    Adding readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+# React with JSPM.io
+
+Simple starter for React with ES6, using jspm.io instead of webpack.
--- components/hero.jsx
+++ components/hero.jsx
@@ -3,10 +3,8 @@ import React from 'react'
 class Hero extends React.Component {
 
     constructor() {
-        // Always call super() as the first thing you do in an extended constructor!
         super()
 
-        // In ES6, initial state is set as a property of the class
         this.state = {
 
         }
