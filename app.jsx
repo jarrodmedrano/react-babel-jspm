56968ae1f63d86310602c9c30e15c734c283e906
Jarrod Medrano (Insight Global) <v-jamedr@microsoft.com>
2015-11-19 12:37:37 -0800

    removing comments

--- app.jsx
+++ app.jsx
@@ -6,31 +6,21 @@ import { Router, Route, Link } from 'react-router'
 import Hero from 'components/hero.jsx!'
 import Login from 'components/login.jsx!'
 
-// Then we delete a bunch of code from App and
-// add some <Link> elements...
 const App = React.createClass({
     render() {
         return (
             <div>
                 <h1>App</h1>
-                {/* change the <a>s to <Link>s */}
                 <ul>
                     <li><Link to="/hero">Hero</Link></li>
                     <li><Link to="/login">Login</Link></li>
                 </ul>
-
-                {/*
-                 next we replace `<Child>` with `this.props.children`
-                 the router will figure out the children for us
-                 */}
                 {this.props.children}
             </div>
         )
     }
 })
 
-// Finally, we render a <Router> with some <Route>s.
-// It does all the fancy routing stuff for us.
 render((
     <Router>
         <Route path="/" component={App}>
